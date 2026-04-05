35907b715d32090cede8d0c49bc3bda8248b1ea6
WeN1nG <3266038380@qq.com>
2026-04-05 22:15:29 +0800

    移除build目录跟踪，添加到.gitignore

--- build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_PersonalDateAssisant_FILE C:/CodeSpace/Objects/QTObj/PersonalDateAssisant/build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/PersonalDateAssisant.exe)
-set(__QT_DEPLOY_TARGET_PersonalDateAssisant_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_PersonalDateAssisant_RUNTIME_DLLS F:/qt/6.9.3/mingw_64/bin/Qt6Widgets.dll;F:/qt/6.9.3/mingw_64/bin/Qt6Sql.dll;F:/qt/6.9.3/mingw_64/bin/Qt6Network.dll;F:/qt/6.9.3/mingw_64/bin/Qt6Gui.dll;F:/qt/6.9.3/mingw_64/bin/Qt6Core.dll)
